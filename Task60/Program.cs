066f0327a4f99e1b459ac1ae2d3ffe776da11864
79081 <79081>
2022-12-30 22:24:15 +0300

    Task60+

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -1,63 +1,39 @@
 ﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
 66(0,0,0) 25(0,1,0)
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 */
-int deep1 = InputInt("Введите размерность 1: ");
-int deep2 = InputInt("Введите размерность 2: ");
-int deep3 = InputInt("Введите размерность 3: ");
-int countNums = 89;
-if (deep1 * deep2 * deep3 > countNums)
+int[,,] array3D = new int[2, 2, 2];
+FillArray(array3D);
+PrintIndex(array3D);
+void PrintIndex(int[,,] arr)
 {
-    Console.Write("Массив слишком большой");
-    return;
-}
-int[,,] resultNums = Create3DMassive(deep1, deep2, deep3);
-
-for (int i = 0; i < resultNums.GetLength(0); i++)
-{
-    for (int j = 0; j < resultNums.GetLength(1); j++)
+    for (int i = 0; i < array3D.GetLength(0); i++)
     {
-        for (int k = 0; k < resultNums.GetLength(2); k++)
+        for (int j = 0; j < array3D.GetLength(1); j++)
         {
-            Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-int[,,] Create3DMassive(int size1, int size2, int size3)
-{
-    int[,,] array = new int[size1, size2, size3];
-    int[] values = new int[countNums];
-    int num
-     = 10;
-    for (int i = 0; i < values.Length; i++)
-        values[i] = num
-        ++;
-    for (int i = 0; i < values.Length; i++)
-    {
-        int randomInd = new Random().Next(0, values.Length);
-        int temp = values[i];
-        values[i] = values[randomInd];
-        values[randomInd] = temp;
-    }
-    int valueIndex = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(2); k++)
+            Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2); k++)
             {
-                array[i, j, k] = values[valueIndex++];
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
             }
         }
     }
-    return array;
 }
-int InputInt(string output)
+void FillArray(int[,,] arr)
 {
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
 }
